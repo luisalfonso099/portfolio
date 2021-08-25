34bf286bd91a02dd6b58ef81f8ffb6fc29811805
Luis <luisalfonso099@gmail.com>
2021-08-25 17:27:41 -0300

    primer commit

---
+++ scrip.js
@@ -0,0 +1,15 @@
+const items = document.getElementById("items");
+const templateCard = document.getElementById("template-card").content;
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
+
+const fetchData = async () => {
+  try {
+    const res = await fetch("api.json");
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
